1b7a3da4837343682225e2bf21fc61fbec9bec53
Jerry Peng <jzp5@cornell.edu>
2022-12-14 18:28:44 -0500

    not working

--- engine.py
+++ engine.py
@@ -7,7 +7,7 @@ from bot_bets import preflop_bet, calculate_bet
 from itertools import combinations
 
 
-def classify(hand: T.List[T.List]) -> int:
+def classify(hand) -> int:
     """
     Classifies a given hand using rules described at https://archive.ics.uci.edu/ml/datasets/Poker+Hand.
 
@@ -22,17 +22,15 @@ def classify(hand: T.List[T.List]) -> int:
     r = 0
     res = 0
     straight = 0
-    for index, atr in enumerate(hand):
-        if index % 2 == 0:
-            if atr in suit:
-                suit[atr] += 1
-            else:
-                suit[atr] = 1
+    for suit_c, rank_c in hand:
+        if suit_c in suit:
+            suit[suit_c] += 1
         else:
-            if atr in rank:
-                rank[atr] += 1
-            else:
-                rank[atr] = 1
+            suit[suit_c] = 1
+        if rank_c in rank:
+            rank[rank_c] += 1
+        else:
+            rank[rank_c] = 1
     s_max = max(suit.values())
     if s_max == 5:
         res = max(res, 5)
@@ -45,7 +43,7 @@ def classify(hand: T.List[T.List]) -> int:
     if rank_list[0] == 1 and rank_list[-1] == 13:
         seq = [1, 10, 11, 12, 13]
     else:
-        seq = [rank_list[0] + i for i in range(5)]
+        seq = [rank_list[0] + str(i) for i in range(5)]
     if rank_list == seq:
         straight = 1
         res = max(res, 4)
@@ -367,7 +365,8 @@ class GameEngine:
                                 self.moveleft_status[i] = True
                         self.bet(position, decision[1])
                     else:
-                        self.fold(position)
+                        # self.fold(position) tmp for testing
+                        self.bet(position, self.curr_bet)
                 elif i == position:
                     self.fold(position)
             self.moveleft_status[position] = False
@@ -386,6 +385,67 @@ class GameEngine:
         self.com_cards += new_cards
         print("Flop: ", str(self.com_cards))
 
+    def check_if_no_moves_left(self, arr: T.List[T.List[bool]]) -> bool:
+        """
+        Check if any players have moves left based on the matrix tracking
+        player statuses.
+
+        Return:
+        True if no moves are left,
+        False if there are possible moves left
+        """
+        for i in range(len(arr)):
+            if arr[i]:
+                return False
+        return True
+
+    def profile_postflop_bet(self, i: int) -> T.List:
+        """
+        Preflop betting for non-AI players (only used during training phase)
+        Logic:
+        We use random numbers to introduce a non-deterministic quality so the
+        bot doesn't become predictable. i represents the range of the preflop
+        quality that the current player's hand is in, with 1 being top and 4 being
+        bottom.
+
+        Parameters:
+        i: rank of player hand that we're betting on
+        """
+        num = np.random.uniform(0, 100, 1)
+        if i == 1:
+            if num <= 95:
+                betorcheck = np.random.uniform(0, 100, 1)
+                if betorcheck <= 90:
+                    bet_variation = np.random.uniform(0, 2, 1)
+                    return ['Bet', self.big_blind * (1 + bet_variation)]
+                else:
+                    return ['Check', 0]
+        elif i == 2:
+            if num <= 75:
+                betorcheck = np.random.uniform(0, 100, 1)
+                if betorcheck <= 60:
+                    bet_variation = np.random.uniform(0, 1, 1)
+                    return ['Bet', self.big_blind * (1 + bet_variation)]
+                else:
+                    return ['Check', 0]
+        elif i == 3:
+            if num <= 50:
+                betorcheck = np.random.uniform(0, 100, 1)
+                if betorcheck <= 30:
+                    bet_variation = np.random.uniform(0, 0.5, 1)
+                    return ['Bet', self.big_blind * (1 + bet_variation)]
+                else:
+                    return ['Check', 0]
+        elif i == 4:
+            if num <= 20:
+                betorcheck = np.random.uniform(0, 100, 1)
+                if betorcheck <= 15:
+                    bet_variation = np.random.uniform(0, 0.25, 1)
+                    return ['Bet', self.big_blind * (1 + bet_variation)]
+                else:
+                    return ['Check', 0]
+        return ['Fold', 0]
+
     def postflop_play(self) -> None:
         """
         Postflop game play
@@ -393,6 +453,7 @@ class GameEngine:
         """
         print("****************PF")
         position = (self.dealer + 2) % self.player_ct
+        bet_in_play = False
         while self.hand_ct > 1 and self.check_if_no_moves_left(self.moveleft_status) == False:
             print("STILL IN PLAY ARRAY IS ", str(self.play_status))
             print("MOVE LEFT ARRAY IS ", str(self.moveleft_status))
@@ -423,14 +484,24 @@ class GameEngine:
             for i in range(1, position+1):
                 options = self.pf_range[i]
                 if encode in options:
-                    # SET BOT TO BE POSITION 0.
+                    # FIX THIS BOT, CANNOT BE PREFLOP BET BOT
                     if position == 0:
                         print("Bot")
                         decision = preflop_bet_bot(i, self.curr_bet, self.pot)
                     else:
-                        decision = self.profile_preflop_bet(i)
+                        if bet_in_play:
+                            decision = self.profile_preflop_bet(i)
+                        else:
+                            decision = self.profile_postflop_bet(i)
                     print("decision: ", str(decision))
-                    if decision[0] == 'Call':
+                    if decision[0] == 'Bet':
+                        bet_in_play = True
+                        for i in range(self.player_ct):
+                            if self.play_status[i]:
+                                self.moveleft_status[i] = True
+                        self.bet(position, decision[1])
+
+                    elif decision[0] == 'Call':
                         self.bet(position, self.curr_bet)
 
                     elif decision[0] == 'Raise':
@@ -439,9 +510,11 @@ class GameEngine:
                                 self.moveleft_status[i] = True
                         self.bet(position, decision[1])
                     else:
-                        self.fold(position)
+                        if bet_in_play == True:
+                            self.fold(position)
                 elif i == position:
-                    self.fold(position)
+                    if bet_in_play == True:
+                        self.fold(position)
             self.moveleft_status[position] = False
 
             print("end_for")
@@ -453,11 +526,30 @@ class GameEngine:
         """
         Evaluate remaining players' hands and return winner
         """
+        scores = [-1 for i in range(self.player_ct)]
+        max_score = -1
+        winner = -1
+        winning_hands = [[] for i in range(self.player_ct)]
         for i in range(self.player_ct):
             if self.play_status[i]:
+                res_max = -1
                 all_cards = self.com_cards + self.player_cards[i]
+                com_tuple = (self.com_cards[0], self.com_cards[1],
+                             self.com_cards[2], self.com_cards[3], self.com_cards[4])
                 com_subsets = list(combinations(all_cards, 5))
-                print(com_subsets)
+                com_subsets.remove(com_tuple)
+                for quint in com_subsets:
+                    res = classify(quint)
+                    if res > res_max:
+                        res_max = res
+                        winning_hands[i] = quint
+                scores[i] = res_max
+                if res_max > max_score:
+                    max_score = res_max
+                    winner = i
+        print(winning_hands)
+        print(scores)
+        print(winner)
 
     def play(self) -> None:
         """
@@ -465,6 +557,7 @@ class GameEngine:
         """
         print(self.com_cards)
         self.preflop_play()
+        print(self.play_status)
         self.postflop_play()
         self.flop(1)
         self.postflop_play()
